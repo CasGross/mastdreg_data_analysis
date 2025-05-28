1c465c09dfeb8ced3ab7a0a173c7bc8151c8db45
CasGross <caspargrossmsa@gmail.com>
2025-05-28 09:12:24 +0200

    push

--- WF_1.5_natürliche Person_vs_gewerbe.py
+++ WF_1.5_natürliche Person_vs_gewerbe.py
@@ -21,3 +21,12 @@ WHERE name_anlagenbetreiber LIKE '%natürliche Person%';
 SELECT * FROM nat_pers_condensed_4 ;
 
 
+CREATE MATERIALIZED VIEW IF NOT EXISTS nat_pers_verteilung_nach_Kapazitaet AS
+SELECT
+    COUNT(*) FILTER (WHERE nutzbare_speicherkapazitaet_kwh >0 AND nutzbare_speicherkapazitaet_kwh <= 5) AS kap_0_5,
+    COUNT(*) FILTER(WHERE nutzbare_speicherkapazitaet_kwh > 5 AND nutzbare_speicherkapazitaet_kwh <= 10) AS kap_5_10,
+    COUNT(*) FILTER (WHERE nutzbare_speicherkapazitaet_kwh >10 AND nutzbare_speicherkapazitaet_kwh <= 15) AS kap_10_15,
+    COUNT(*) FILTER (WHERE nutzbare_speicherkapazitaet_kwh >15 AND nutzbare_speicherkapazitaet_kwh <= 20) AS kap_15_20,
+    COUNT(*) FILTER (WHERE nutzbare_speicherkapazitaet_kwh >20 AND nutzbare_speicherkapazitaet_kwh <= 25) AS kap_20_25,
+    COUNT(*) FILTER (WHERE nutzbare_speicherkapazitaet_kwh >25) AS kap_25_plus
+FROM nat_pers_condensed_4
