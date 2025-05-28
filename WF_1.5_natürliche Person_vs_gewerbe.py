98f689ec143a0dfe10b4dad370f712b99e62ad52
CasGross <caspargrossmsa@gmail.com>
2025-05-28 09:39:43 +0200

    nat.Pers vs gesamt

--- WF_1.5_natürliche Person_vs_gewerbe.py
+++ WF_1.5_natürliche Person_vs_gewerbe.py
@@ -23,10 +23,30 @@ SELECT * FROM nat_pers_condensed_4 ;
 
 CREATE MATERIALIZED VIEW IF NOT EXISTS nat_pers_verteilung_nach_Kapazitaet AS
 SELECT
-    COUNT(*) FILTER (WHERE nutzbare_speicherkapazitaet_kwh >0 AND nutzbare_speicherkapazitaet_kwh <= 5) AS kap_0_5,
-    COUNT(*) FILTER(WHERE nutzbare_speicherkapazitaet_kwh > 5 AND nutzbare_speicherkapazitaet_kwh <= 10) AS kap_5_10,
+    COUNT(*) FILTER (WHERE nutzbare_speicherkapazitaet_kwh >0  AND nutzbare_speicherkapazitaet_kwh <= 5)  AS kap_0_5,
+    COUNT(*) FILTER (WHERE nutzbare_speicherkapazitaet_kwh > 5 AND nutzbare_speicherkapazitaet_kwh <= 10) AS kap_5_10,
     COUNT(*) FILTER (WHERE nutzbare_speicherkapazitaet_kwh >10 AND nutzbare_speicherkapazitaet_kwh <= 15) AS kap_10_15,
     COUNT(*) FILTER (WHERE nutzbare_speicherkapazitaet_kwh >15 AND nutzbare_speicherkapazitaet_kwh <= 20) AS kap_15_20,
     COUNT(*) FILTER (WHERE nutzbare_speicherkapazitaet_kwh >20 AND nutzbare_speicherkapazitaet_kwh <= 25) AS kap_20_25,
-    COUNT(*) FILTER (WHERE nutzbare_speicherkapazitaet_kwh >25) AS kap_25_plus
-FROM nat_pers_condensed_4
+    COUNT(*) FILTER (WHERE nutzbare_speicherkapazitaet_kwh >25) 											AS kap_25_plus
+FROM nat_pers_condensed_4;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS gesamt_verteilung_nach_Kapazitaet AS
+SELECT
+    COUNT(*) FILTER (WHERE nutzbare_speicherkapazitaet_kwh >0  AND nutzbare_speicherkapazitaet_kwh <= 5)  AS kap_0_5,
+    COUNT(*) FILTER (WHERE nutzbare_speicherkapazitaet_kwh >5  AND nutzbare_speicherkapazitaet_kwh <= 10) AS kap_5_10,
+    COUNT(*) FILTER (WHERE nutzbare_speicherkapazitaet_kwh >10 AND nutzbare_speicherkapazitaet_kwh <= 15) AS kap_10_15,
+    COUNT(*) FILTER (WHERE nutzbare_speicherkapazitaet_kwh >15 AND nutzbare_speicherkapazitaet_kwh <= 20) AS kap_15_20,
+    COUNT(*) FILTER (WHERE nutzbare_speicherkapazitaet_kwh >20 AND nutzbare_speicherkapazitaet_kwh <= 25) AS kap_20_25,
+    COUNT(*) FILTER (WHERE nutzbare_speicherkapazitaet_kwh >25) 											AS kap_25_plus
+FROM einheiten_stromspeicher_ungefiltert_1;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS nat_pers_vs_gesamt_verteilung_nach_Kapazitaet_4 AS
+SELECT *, 'nat_Pers' AS Art_der_Einheit
+FROM nat_pers_verteilung_nach_Kapazitaet
+UNION ALL
+SELECT *, 'gesamt' AS Art_der_Einheit
+FROM gesamt_verteilung_nach_Kapazitaet;
+
+SELECT * FROM nat_pers_vs_gesamt_verteilung_nach_Kapazitaet_4;
+
